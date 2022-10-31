b00970a6c0ccfb25f8462245d0214aa8cbc4aaf0
jdaniels98 <jackdanielswork98@gmail.com>
2022-10-31 15:00:59 +0000

    map function and useEffect

--- src/App.js
+++ src/App.js
@@ -1,14 +1,91 @@
-import React from 'react';
 import './App.css';
 import Box from './components/box';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    fetchImages()
+  },[])
+
   const [user, setUser] = useState()
   const [age, setAge] = useState()
+  const [photos, setPhotos] = useState([])
+  // const [username, setUsername] = useState()
+  // const [email, setEmail] = useState()
+  // const [password, setPassword] = useState()
+
+  // const submitHandler = async (event) => {
+  //   event.preventDefault()
+  //   console.log(username,email,password)
+  // }
+
+  const array = [{}]
+
+  // for (let i = 0; i < array.length; i++) {
+  //   console.log(array[i])
+  // }
+
+  // array.map((item, index) => {
+  //   console.log(index, item)
+  // })
+
+  const fetchImages = async () => {
+    const response = await fetch ("https://picsum.photos/v2/list")
+    const data = await response.json()
+    setPhotos(data)
+  }
 
   return (
     <div className="App">
+      {array.map((item, index) => {
+        return (
+          <div>
+            <Box name={item.name} age={item.age} />
+          </div>
+        )
+      })}
+
+      {/* <button onClick = {(event) => fetchImages()}>Fetch Images</button> */}
+
+      {photos.map((item, index) => {
+        return (
+          <div>
+            <img src={item.download_url} width="200px" />
+            <h2>{item.author}</h2>
+          </div>
+        )
+      })}
+
+
+      {/* <img src="https://picsum.photos/v2/list" alt="a random photo from lorem picsum" /> */}
+
+
+
+
+
+
+
+
+
+
+
+
+
+      {/* <form onSubmit ={submitHandler}>
+        <label>Username:
+          <input onChange = {(event) => setUsername(event.target.value)} />
+        </label>
+        <br></br>
+        <label>Email:
+          <input type="email" onChange = {(event) => setEmail(event.target.value)} />
+        </label>
+        <br></br>
+        <label>Password:
+          <input onChange = {(event) => setPassword(event.target.value)} />
+        </label>
+        <br></br>
+        <button type="submit">Click here to sign-up or login!</button>
+      </form>
       <input onChange={(event) => setUser(event.target.value)} />
       <input onChange={(event) => setAge(event.target.value)} />
       {user ?
@@ -20,31 +97,7 @@ function App() {
       </div>
       : <div>
         <h1>this is the else part of the statement</h1>
-        </div>}
-        <div>
-          <form>
-            <label>
-              Username:
-              <input type="text" username="username" />
-            </label>
-          </form>
-        </div>
-        <div>
-          <form>
-            <label>
-              Email Address:
-              <input type="text" username="username" />
-            </label>
-          </form>
-        </div>
-        <div>
-          <form>
-            <label>
-              Password:
-              <input type="text" username="username" />
-            </label>
-          </form>
-        </div>
+        </div>} */}
     </div>
   );
 }
