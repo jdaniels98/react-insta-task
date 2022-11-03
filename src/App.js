7d8ef5ea36347682450c1de9df25117012ef354b
jdaniels98 <jackdanielswork98@gmail.com>
2022-11-03 09:10:55 +0000

    add structure for update and delete components

--- src/App.js
+++ src/App.js
@@ -10,19 +10,8 @@ const App = () => {
   },[])
   
   const [user, setUser] = useState()
-  // const [username, setUsername] = useState()
-  // const [email, setEmail] = useState()
-  // const [password, setPassword] = useState()
-  // const [age, setAge] = useState()
   const [photos, setPhotos] = useState([])
   const [isLoaded, setIsLoaded] = useState(false)
-
-  // const submitHandler = async (event) => {
-  //   event.preventDefault()
-  //   console.log(username,email,password)
-  // }
-
-  // const array = [{}]
 
   const fetchImages = async () => {
     const response = await fetch ("https://picsum.photos/v2/list")
@@ -33,13 +22,6 @@ const App = () => {
 
   return (
     <div className="App">
-      {/* {array.map((item, index) => {
-        return (
-          <div>
-            <Box name={item.name} age={item.age} />
-          </div>
-        )
-      })} */}
 
       <Register setter={setUser} />
       {user ?
@@ -49,33 +31,6 @@ const App = () => {
       </div>
       :
       <h2>Please register an account with us!</h2>}
-
-
-{/* <form onSubmit ={submitHandler}>
-        <label>Username:
-          <input onChange = {(event) => setUsername(event.target.value)} />
-        </label>
-        <br></br>
-        <label>Email:
-          <input type="email" onChange = {(event) => setEmail(event.target.value)} />
-        </label>
-        <br></br>
-        <label>Password:
-          <input onChange = {(event) => setPassword(event.target.value)} />
-        </label>
-        <br></br>
-        <button type="submit">Click here to sign-up or login!</button>
-      </form> */}
-      {/* <input onChange={(event) => setUser(event.target.value)} />
-      <input onChange={(event) => setAge(event.target.value)} />
-      {user ?
-      <div><h1>Test Header</h1>
-      <Box name={user} age={age} />
-      </div>
-      : <div>
-        <h1>this is the else part of the statement</h1>
-        </div>} */}
-
 
       {isLoaded && photos.map((item, index) => {
         return (
--- src/utils/index.js
+++ src/utils/index.js
@@ -24,11 +24,44 @@ export const readUsers = async () => {
             headers: {"Content-Type": "application/json"}
         })
         const data = await response.json()
-        // console.log(data)
         const usernames = data.user.map(users => users.username)
-        // console.log(usernames)
         return usernames
     } catch (error) {
         console.log(error)
     }
 }
+
+export const updateUser = async (username, key, value) => {
+    try {
+        const response = await fetch("http://localhost:5001/updateUser", {
+            method: "PUT",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({
+                "username": username,
+                "key": key,
+                "value": value
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+    } catch (error){
+        console.log(error)
+    }
+}
+
+export const deleteUser = async (username) => {
+    try {
+        const response = await fetch("http://localhost:5001/deleteUser", {
+            method: "DELETE",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({
+                "username": username
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
